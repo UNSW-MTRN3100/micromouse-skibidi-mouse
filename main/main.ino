0f2e18e77b4ee12b69e9ffa07bd03c033ff341bf
TriDoHuu <146409357+TriDoHuu@users.noreply.github.com>
2024-08-13 18:37:09 +1000

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,5 @@
+
+
 // Easy search: COMPONENTS, ACTION, TODO
 #include "libs.hpp"
 #include <MPU6050_light.h>
@@ -5,21 +7,26 @@
 #include <VL6180X.h>
 #include <Wire.h>
 
+
 // ACTION: Intialise the sensors
 VL6180X sensor1;
 VL6180X sensor2;
 VL6180X sensor3;
 
+
 int sensor1_pin = A0; // ENABLE PIN FOR SENSOR 1
 int sensor2_pin = A1; // ENABLE PIN FOR SENSOR 2
 int sensor3_pin = A2; // ENABLE PIN FOR SENSOR 3
+
 
 // ACTION: Intialise variables for the yaw MPU6050
 unsigned long timer = 0;
 float gyroZ = 0;
 float yaw = 0;
 
+
 MPU6050 mpu(Wire);
+
 
 // COMPONENTS: Encoders pin
 #define EN_1_A 2 //These are the pins for the PCB encoder
@@ -27,11 +34,13 @@ MPU6050 mpu(Wire);
 #define EN_2_A 3 //These are the pins for the PCB encoder
 #define EN_2_B 8 //These are the pins for the PCB encoder
 
+
 // COMPONENTS: Motor pin
 #define MOT_1_PWM 11 //These are pins for the motors
 #define MOT_1_DIR 12 //These are pins for the motors
 #define MOT_2_PWM 9 //These are pins for the motors
 #define MOT_2_DIR 10 //These are pins for the motors
+
 
 // ACTION: Initialise dual encoder, IMU
 mtrn3100::DualEncoder encoder(EN_1_A, EN_1_B,EN_2_A, EN_2_B);
@@ -39,54 +48,65 @@ mtrn3100::DualEncoder encoder(EN_1_A, EN_1_B,EN_2_A, EN_2_B);
 mtrn3100::EncoderOdometry encoder_odometry(31.73/2, 104/2); //TASK1 TODO: IDENTIFY THE WHEEL RADIUS AND AXLE LENGTH
 mtrn3100::IMUOdometry IMU_odometry;
 
+
 // ACTION: Motor initialise
 mtrn3100::Motor motor1(MOT_1_PWM, MOT_1_DIR);
 mtrn3100::Motor motor2(MOT_2_PWM, MOT_2_DIR);
 
+
 // ACTION: Bangbang controller initialise
 // TODO: Tune the value
-mtrn3100::BangBangController controller(178, 4); // in PWM
-mtrn3100::BangBangController controllerR(175/3, 3); // in deg
-mtrn3100::BangBangController controllerL(175/3, 3); // in deg
+mtrn3100::BangBangController controller(120, 4); // in PWM
+mtrn3100::BangBangController controllerR(125/3, 3); // in deg
+mtrn3100::BangBangController controllerL(125/3, 3); // in deg
+
 
 // ACTION: PIDController initialise
 // TODO: Tune the value
-// mtrn3100::PIDController controller(100, 10, 6); // in PWM
+//mtrn3100::PIDController controller(100, 0, 30); // in PWM
 //mtrn3100::PIDController controllerR(50, 0, 0); // in rad
 //mtrn3100::PIDController controllerL(50, 0, 0); // in rad
+
 
 int firstLeftWall = 0;
 int firstRightWall = 0;
 
+
 void setup() {
   // ACTION: Setup the LiDar
   lidarSetup();
-  
+ 
   // ACTION: Setup the terminal
   serialSetup();
 
+
   // ACTION: Set up the IMU
    mpuSetup();
+
 
   // ACTION: Setup the BangbangController
   controllerSetup();
   Serial.println("Setup function end");
 }
 
+
 void loop() {
   delay(50);
 
+
   // ACTION: Test Motor, Odometry, IMU and Bang Bang
-  // TODO: Test this 
+  // TODO: Test this
 //   Serial.println(encoder.getLeftRotation());
 //   Serial.println(encoder.getRightRotation());
 
+
   // ACTION: Read in commands and processing
   // TODO: Test this
-  // String commands = "flffrfflffrfrffff";
-  String commands = "fffffff";
+  String commands = "ffflff";
+//String commands = "ffrrfrfl";
   processCommands(commands);
 }
+
 
 void controllerSetup() {
   controller.zeroAndSetTarget(0, 250); // in mm
@@ -94,9 +114,11 @@ void controllerSetup() {
   controllerR.zeroAndSetTarget(0, -90); // in mm
 }
 
+
 void serialSetup() {
     Serial.begin(115200);  
 }
+
 
 void lidarSetup() {
   Wire.begin();
@@ -108,7 +130,8 @@ void lidarSetup() {
   digitalWrite(sensor2_pin, LOW);
   digitalWrite(sensor3_pin, LOW);
 
-  // ENABLE FIRST SENSOR AND CHANGE THE ADDRESS 
+
+  // ENABLE FIRST SENSOR AND CHANGE THE ADDRESS
   digitalWrite(sensor1_pin, HIGH);
   delay(50);
   sensor1.init();
@@ -118,9 +141,11 @@ void lidarSetup() {
   delay(50);
 
 
-  
-  // ENABLE SECOND SENSOR AND CHANGE THE ADDRESS 
-  // NOTE: WE DO NOT HAVE TO DISABLE THE FIRST SENSOR AS IT IS NOW ON A DIFFERENT ADDRESS 
+
+
+ 
+  // ENABLE SECOND SENSOR AND CHANGE THE ADDRESS
+  // NOTE: WE DO NOT HAVE TO DISABLE THE FIRST SENSOR AS IT IS NOW ON A DIFFERENT ADDRESS
   digitalWrite(sensor2_pin, HIGH);
   delay(50);
   sensor2.init();
@@ -129,7 +154,8 @@ void lidarSetup() {
   sensor2.setAddress(0x62);
   delay(50);
 
-  // ENABLE THIRD SENSOR AND CHANGE THE ADDRESS 
+
+  // ENABLE THIRD SENSOR AND CHANGE THE ADDRESS
   digitalWrite(sensor3_pin, HIGH);
   delay(50);
   sensor3.init();
@@ -138,9 +164,11 @@ void lidarSetup() {
   sensor3.setAddress(0x58);
   delay(50);
 
+
   firstLeftWall = 80;
   firstRightWall = 80;
 }
+
 
 void mpuSetup() {
   Wire.begin();
@@ -148,12 +176,13 @@ void mpuSetup() {
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);
   while(status!=0){ } // stop everything if could not connect to MPU6050
-  
+ 
   Serial.println(F("Calculating offsets, do not move MPU6050"));
   delay(500);
   mpu.calcOffsets(true,true);
   Serial.println("Done!\n");
 }
+
 
 void processCommands(String commands) {
     for (char command : commands) {
@@ -173,7 +202,7 @@ void processCommands(String commands) {
         encoder_odometry.reset();
         break;
       case 'r':
-        Serial.println("Turn right"); 
+        Serial.println("Turn right");
         turnRight(-88, 3);
         driveStop();  
         encoder_odometry.reset();
@@ -182,35 +211,40 @@ void processCommands(String commands) {
     delay(50);
   }
 
+
   while(true) {
     Serial.println("Finishing command");
     driveStop();
     delay(1000);
-    
+   
   }
 }
+
 
 void driveStraight() {
   Serial.println("Driving Straight");
 
+
   // ACTION: Get the currYaw
   float startingYaw = getYawMPU();
 
-  
+
+ 
   // ACTION: Set checkpoint
   controller.zeroAndSetTarget(encoder_odometry.getX(), 250); // in mm
   // ACTION: Computing current state
   controller.compute(encoder_odometry.getX());
 
+
   // TODO: Tune the error
   // ACTION: Check if it's adjusted
-  while (fabs(controller.getError()) > 12) {
+  while (fabs(controller.getError()) > 30) {
     Serial.print("The error is ");
     Serial.println(controller.getError());
     // ACTION: Update postition
     encoder_odometry.update(encoder.getLeftRotation(),encoder.getRightRotation());
     displayEncoderOdom(encoder_odometry.getX(), encoder_odometry.getH());
-    
+   
     // ACTION: Comput the control signal
     int controlSignal = controller.compute(encoder_odometry.getX());
     // Bangbang controller
@@ -218,9 +252,11 @@ void driveStraight() {
     Serial.print("The calculated speed is: ");
     Serial.println(controlSignal);
 
+
     float leftWall = sensor1.readRangeSingleMillimeters();
     float rightWall = sensor3.readRangeSingleMillimeters();
     float frontWall = sensor2.readRangeSingleMillimeters();
+
 
     if (frontWall < 85) {
       break;
@@ -229,16 +265,18 @@ void driveStraight() {
     if (leftWall < 79) {
       // Turn left
       motor1.setPWM(-controlSignal);
-      motor2.setPWM(controlSignal - 20);
+      motor2.setPWM(controlSignal - 5);
       continue;
-    } 
+    }
+
 
     if (rightWall < 79) {
       // Turn Right
-      motor1.setPWM(-controlSignal + 20);
+      motor1.setPWM(-controlSignal + 5);
       motor2.setPWM(controlSignal);
       continue;
     }
+
 
 //    if(rightWall > 81 && leftWall > 81) {
 //      float yaw = getYawMPU();
@@ -254,27 +292,33 @@ void driveStraight() {
 //    }
 
 
-    
+
+
+   
     // ACTION: Use the Control Signal to calculate
     straight(controlSignal);
   }
-  
+ 
 }
+
 
 void straight(int pwm) {
   motor1.setPWM(-pwm);
   motor2.setPWM(pwm);
 }
 
+
 void turnLeft(float degree, float error){
-  
+ 
   // ACTION: Set checkpoint
   controllerL.zeroAndSetTarget(getYawMPU(), degree); // in mm
   // ACTION: Computing current state
   controllerL.compute(getYawMPU());
 
+
   Serial.print("The error is ");
   Serial.println(controllerL.getError());
+
 
   // TODO: Tune the error
   while (fabs(controllerL.getError()) > error) {
@@ -283,21 +327,22 @@ void turnLeft(float degree, float error){
     // ACTION: Update postition
     float currYaw = getYawMPU();
     controllerL.compute(currYaw);
-    
+   
     // ACTION: Comput the control signal
     int controlSignal = controllerL.compute(currYaw);
     // Bangbang controller
     // TODO: Test this
     Serial.print("The calculated speed is: ");
     Serial.println(controlSignal);
-    
+   
     // ACTION: Use the Control Signal to calculate
     motor1.setPWM(controlSignal);
     motor2.setPWM(controlSignal);
   }
    encoder.reset();
-  
+ 
 }
+
 
 void turnRight(float degree, float error) {
   // ACTION: Set checkpoint
@@ -305,8 +350,10 @@ void turnRight(float degree, float error) {
   // ACTION: Computing current state
   controllerR.compute(getYawMPU());
 
+
   Serial.print("The error is ");
   Serial.println(controllerR.getError());
+
 
   // TODO: Tune the error
   while (fabs(controllerR.getError()) > error) {
@@ -321,6 +368,7 @@ void turnRight(float degree, float error) {
     Serial.print("The calculated speed is: ");
     Serial.println(controlSignal);
 
+
     // ACTION: Use the Control Signal to calculate
     motor1.setPWM(controlSignal);
     motor2.setPWM(controlSignal);
@@ -328,28 +376,34 @@ void turnRight(float degree, float error) {
   encoder.reset();
 }
 
+
 void driveStop() {
   motor1.setPWM(0);
   motor2.setPWM(0);
 }
 
+
 void test() {
-  // ACTION: Get rotation from the IMU 
+  // ACTION: Get rotation from the IMU
   // TODO: It works if we do it slow/fast enough
   // TODO: Tune the alpha
   testMPU();
 
+
   Serial.println("Test MPU ending");
   // ACTION: Run the motor
-  // TODO: Test this 
+  // TODO: Test this
   testMotor();
+
 
   // ACTION: Test Encoder Odometry and Bang Bang
   testEncoderAndController();
 
+
   // ACTION: Test LiDar
   testLiDar();
 }
+
 
 void testLiDar() {
   Serial.println("LiDar Reading.....");
@@ -368,21 +422,24 @@ void testLiDar() {
   delay(50);
 }
 
+
 void testMPU() {
   getYawMPU();
 }
+
 
 //float data[100] = {0};
 float getYawMPU() {
   mpu.update();
   gyroZ = mpu.getGyroZ();
   float dt = (millis() - timer) / 1000.0;
-  yaw += kalmanFilter(gyroZ) * dt; 
+  yaw += kalmanFilter(gyroZ) * dt;
   timer = millis();
   Serial.print("Yaw: ");
   Serial.println(yaw);
   return yaw;
 }
+
 
 float kalmanFilter(float U) {
   static const double R = 500;
@@ -392,47 +449,55 @@ float kalmanFilter(float U) {
   static double U_hat = 0;
   static double K = 0;
 
+
   K = P*H/(H*P*H + R);
   U_hat = U_hat + K*(U - H*U_hat);
   P = (1-K*H)*P + Q;
   return U_hat;
 }
 
+
 void testMotor() {
   Serial.println("Both wheels move the same time");
   motor1.setPWM(255);
   motor2.setPWM(-255);
 
+
   delay(2000);
+
 
   Serial.println("Both wheels move opposite of each other");
   motor1.setPWM(255);
   motor2.setPWM(255);
   delay(2000);
 
+
   Serial.println("Both wheels move opposite of each other (opposite)");
   motor1.setPWM(-255);
   motor2.setPWM(-255);
   delay(2000);
 
+
   Serial.println("Both wheels move the same as each other (opposite)");
   motor1.setPWM(-255);
   motor2.setPWM(255);
   delay(2000);
-  
+ 
 }
+
 
 void testEncoderAndController() {
   // Bangbang controller
   // TODO: Test this
   Serial.print("The calculated speed is: ");
   Serial.println(controller.compute(encoder_odometry.getX()));
-  
+ 
   // Calculate Odom through enocders
   // TODO: change the smapling time?
   encoder_odometry.update(encoder.getLeftRotation(),encoder.getRightRotation());
   displayEncoderOdom(encoder_odometry.getX(), encoder_odometry.getH());
 }
+
 
 void displayEncoderOdom(float x, float h) {
   Serial.print("ODOM:\t\t x: ");
